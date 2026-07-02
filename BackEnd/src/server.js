a90aa8fd9d351943e4fd879bbff35c685ddedf20
mdomarfaruksiam <m.omarfaruksiam@gmail.com>
2026-07-02 16:18:59 +0600

    working for perfection

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -14,18 +14,12 @@ const signIn = require("./routes/sign-in");
 const signOut = require("./routes/sign-out");
 
 
-const hostName = process.env.HOSTNAME || "0.0.0.0";
-const port = process.env.PORT || 5000;
+const hostName = "0.0.0.0";
+const port = 5000;
 
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://192.168.0.104:5173",
-        credentials: true,
-    })
-);
 
 app.get("/", (req, res) => {
     res.status(200).json({
--- FrontEnd/src/Essentials/links.jsx
+++ FrontEnd/src/Essentials/links.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const links = {
-    serverName: 'http://192.168.0.104:5000',
+    serverName: 'https://arbank.onrender.com/',
 }
 
 export default links
