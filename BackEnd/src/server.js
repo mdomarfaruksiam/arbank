89d55fc31650d1170a367b52d696053c7e2bb968
mdomarfaruksiam <m.omarfaruksiam@gmail.com>
2026-07-02 16:33:01 +0600

    working for perfection

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -22,11 +22,10 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: [
-            "http://localhost:5173",
-            "http://192.168.0.104:5173",
-        ],
+        origin: true,
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
