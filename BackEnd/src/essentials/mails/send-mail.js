16b968a4ae9bda5d92473d7f6132c029dc949f7f
mdomarfaruksiam <m.omarfaruksiam@gmail.com>
2026-07-05 10:38:14 +0600

    problem occured for the sending mail with online

--- BackEnd/src/essentials/mails/send-mail.js
+++ BackEnd/src/essentials/mails/send-mail.js
@@ -1,17 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD,
     },
 });
 
+transporter.verify((error) => {
+    if (error) {
+        console.error("SMTP Error:", error);
+    } else {
+        console.log("SMTP Server Connected");
+    }
+});
+
 const sendEmail = async ({ to, subject, text, html }) => {
-    await transporter.sendMail({
-        from: `"ArBank" <${process.env.EMAIL}>`,
+    return transporter.sendMail({
+        from: `"ARBank" <${process.env.EMAIL}>`,
         to,
         subject,
         text,
