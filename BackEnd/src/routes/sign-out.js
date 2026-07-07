43d231d2a1700e3df078552912382b5d7b03e7e0
mdomarfaruksiam <m.omarfaruksiam@gmail.com>
2026-07-08 01:03:19 +0600

    sign out problem solved

--- BackEnd/src/routes/sign-out.js
+++ BackEnd/src/routes/sign-out.js
@@ -3,15 +3,16 @@ const express = require('express')
 const signOut = express.Router()
 
 signOut.post('/sign-out', (req, res) => {
-
-    Object.keys(req.cookies).forEach((cookieName) => {
-        res.clearCookie(cookieName)
-    })
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+    });
 
     res.json({
         success: true,
-        message: 'All cookies cleared.',
-    })
-})
+        message: 'Signed out successfully.',
+    });
+});
 
 module.exports = signOut
